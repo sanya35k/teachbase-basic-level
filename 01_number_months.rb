78e115fc99cb558d5569f777b23d1e6841ef1391
sanya35k <serhii.saltan@gotoinc.co>
2021-12-07 11:44:26 +0200

    TeachBase Lesson02 Task01 Number of months

---
+++ 01_number_months.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class NumberMonths
+  months = {
+    'January' => 31,
+    'February' => 28,
+    'March' => 31,
+    'April' => 30,
+    'May' => 31,
+    'June' => 30,
+    'July' => 31,
+    'August' => 31,
+    'September' => 30,
+    'October' => 31,
+    'November' => 30,
+    'December' => 31
+  }
+
+  months.each { |k, v| puts k if v == 30 }
+end
