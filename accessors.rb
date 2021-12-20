caeafa0f531be79464780259d797252398dbfa01
sanya35k <serhii.saltan@gotoinc.co>
2021-12-20 14:32:07 +0200

    TeachBase Lesson09 Created module Accessors

---
+++ accessors.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Accessors
+  def attr_accessor_with_history(*names)
+    names.each do |name|
+      var_name = "@#{name}".to_sym
+      define_method(name) { instance_variable_get(var_name) }
+      define_method("#{name}=".to_sym) do |value|
+        inst_var_set(var_name, name, value)
+      end
+      define_method("#{name}_history") { @history ? @history[name] : [] }
+    end
+  end
+
+  def inst_var_set(var_name, name, value)
+    instance_variable_set(var_name, value)
+    @history ||= {}
+    @history[name] ||= []
+    @history[name] << value
+  end
+
+  def strong_attr_accessor(name, v_class)
+    var_name = "@#{name}".to_sym
+
+    define_method(name) { instance_variable_get(var_name) }
+
+    define_method("#{name}=") do |value|
+      raise 'Error! Type mismatch!' unless value.is_a?(v_class)
+
+      instance_variable_set(var_name, value)
+    end
+  end
+end
